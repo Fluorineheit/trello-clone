5ef41e6269c62737b7320f0a12e4481aa50b12af
Farhan AP <Farhanadika7@gmail.com>
2025-05-12 17:06:59 +0700

    adding card element

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="p-5 font-sans">
+    <div class="flex gap-5 py-5 overflow-x-auto">
+      <div class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
+        <h2 class="font-medium text-md text-gray-500">The List</h2>
+        <div class="bg-white p-3 my-2 rounded cursor-pointer">
+          <span class="text-md font-medium">Tugas 1</span>
+          <p class="text-sm text-gray-500">Deskripsi tugas 1</p>
+        </div>
+        <button class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">+ Add Card</button>
+      </div>
+      <div class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
+        <h2 class="font-medium text-md text-gray-500">The List</h2>
+        <div class="bg-white p-3 my-2 rounded cursor-pointer">
+          <span class="text-md font-medium">Tugas 1</span>
+          <p class="text-sm text-gray-500">Deskripsi tugas 1</p>
+        </div>
+        <button class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">+ Add Card</button>
+      </div>
+      <div class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
+        <h2 class="font-medium text-md text-gray-500">The List</h2>
+        <div class="bg-white p-3 my-2 rounded cursor-pointer">
+          <span class="text-md font-medium">Tugas 1</span>
+          <p class="text-sm text-gray-500">Deskripsi tugas 1</p>
+        </div>
+        <button class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">+ Add Card</button>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>

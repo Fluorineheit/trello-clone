003e12ff72447843d0313a218bc8205945ab1455
Farhan AP <Farhanadika7@gmail.com>
2025-05-12 17:19:59 +0700

    add reactive list of cards

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,56 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+interface Card {
+  id: number
+  title: string
+  description: string
+}
+
+interface List {
+  id: number;
+  title: string;
+  cards: Card[]
+}
+
+const lists = reactive < List[] > ([
+  {
+    id: 1,
+    title: 'To Do',
+    cards: [
+      { id: 1, title: 'Tugas 1', description: 'Deskripsi tugas 1' },
+      { id: 2, title: 'Tugas 6', description: 'Deskripsi tugas 1' },
+    ],
+  },
+  {
+    id: 2,
+    title: 'In Progress',
+    cards: [
+      { id: 3, title: 'Tugas 2', description: 'Deskripsi tugas 2' },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Done',
+    cards: [
+      { id: 2, title: 'Tugas 3', description: 'Deskripsi tugas 3' },
+    ],
+  },
+])
+</script>
+
 <template>
   <main class="p-5 font-sans">
     <div class="flex gap-5 py-5 overflow-x-auto">
-      <div class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
-        <h2 class="font-medium text-md text-gray-500">The List</h2>
-        <div class="bg-white p-3 my-2 rounded cursor-pointer">
-          <span class="text-md font-medium">Tugas 1</span>
-          <p class="text-sm text-gray-500">Deskripsi tugas 1</p>
+      <div :key="list.id" v-for="list in lists" class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
+        <h2 class="font-medium text-md text-gray-500">{{ list.title }}</h2>
+        <div :key="card.id" v-for="card in list.cards" class="bg-white p-3 my-2 rounded cursor-pointer">
+          <span class="text-md font-medium">{{card.title}}</span>
+          <p class="text-sm text-gray-500">{{ card.description }}</p>
         </div>
-        <button class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">+ Add Card</button>
-      </div>
-      <div class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
-        <h2 class="font-medium text-md text-gray-500">The List</h2>
-        <div class="bg-white p-3 my-2 rounded cursor-pointer">
-          <span class="text-md font-medium">Tugas 1</span>
-          <p class="text-sm text-gray-500">Deskripsi tugas 1</p>
-        </div>
-        <button class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">+ Add Card</button>
-      </div>
-      <div class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
-        <h2 class="font-medium text-md text-gray-500">The List</h2>
-        <div class="bg-white p-3 my-2 rounded cursor-pointer">
-          <span class="text-md font-medium">Tugas 1</span>
-          <p class="text-sm text-gray-500">Deskripsi tugas 1</p>
-        </div>
-        <button class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">+ Add Card</button>
+        <button class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">
+          + Add Card
+        </button>
       </div>
     </div>
   </main>

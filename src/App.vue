795907f68b9ad705d42f470d9531b6323ad05c9f
Farhan AP <Farhanadika7@gmail.com>
2025-06-23 15:11:48 +0700

    add card and edit logic

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
-import draggable from 'vuedraggable';
-import ModalDialog from './components/ModalDialog.vue';
-import type { List } from './types';
-const isModalOpen = ref(false)
-
-const openModal = () => {
-  isModalOpen.value = true
-}
-
-const closeModal = () => {
-  console.log('close modal')
-  isModalOpen.value = false
-}
+import { computed, reactive, ref } from 'vue'
+import draggable from 'vuedraggable'
+import ModalDialog from './components/ModalDialog.vue'
+import type { Card, List } from './types'
 
 const lists = reactive<List[]>([
   {
@@ -21,46 +11,101 @@ const lists = reactive<List[]>([
     cards: [
       { id: 1, title: 'Tugas 1', description: 'Deskripsi tugas 1' },
       { id: 2, title: 'Tugas 6', description: 'Deskripsi tugas 1' },
+      { id: 3, title: 'Tugas 1', description: 'Deskripsi tugas 1' },
+      { id: 4, title: 'Tugas 6', description: 'Deskripsi tugas 1' },
+      { id: 5, title: 'Tugas 1', description: 'Deskripsi tugas 1' },
+      { id: 6, title: 'Tugas 6', description: 'Deskripsi tugas 1' },
     ],
   },
   {
     id: 2,
     title: 'In Progress',
-    cards: [
-      { id: 3, title: 'Tugas 2', description: 'Deskripsi tugas 2' },
-    ],
+    cards: [{ id: 7, title: 'Tugas 2', description: 'Deskripsi tugas 2' }],
   },
   {
     id: 3,
     title: 'Done',
-    cards: [
-      { id: 4, title: 'Tugas 3', description: 'Deskripsi tugas 3' },
-    ],
+    cards: [{ id: 8, title: 'Tugas 3', description: 'Deskripsi tugas 3' }],
   },
 ])
 
+const isModalOpen = ref(false)
+const editingCard = ref<Card | null>(null)
+const editinglistIndex = ref<number | null>(null)
+
+const modalMode = computed(() => {
+  return editingCard.value === null ? 'add' : 'edit'
+})
+
+const openModal = (listIndex: number, card?: Card) => {
+  editinglistIndex.value = listIndex
+  editingCard.value = card === undefined ? null : card
+  isModalOpen.value = true
+}
+
+const saveCard = (card: Card) => {
+  if (editinglistIndex.value === null) {
+    return
+  }
+
+  if (modalMode.value === 'add') {
+    // adding
+    const newId = Math.max(...lists.flatMap(list => list.cards.map(c => c.id)))
+    lists[editinglistIndex.value].cards.push({...card, id: newId})
+  } else {
+    // editing
+    const cardIndex = lists[editinglistIndex.value].cards.findIndex(
+      (cardOnList) => cardOnList.id === card.id,
+    )
+
+    if (cardIndex !== -1){
+      lists[editinglistIndex.value].cards[cardIndex] = card
+    }
+  }
+
+  closeModal()
+}
+
+const closeModal = () => {
+  isModalOpen.value = false
+  editingCard.value = null
+  editinglistIndex.value = null
+}
 </script>
 
 <template>
   <main class="p-5 font-sans">
     <div class="flex gap-5 py-5 overflow-x-auto">
-      <div :key="list.id" v-for="list in lists" class="bg-gray-100 p-5 rounded-lg min-w-[250px] flex flex-col">
+      <div
+        :key="list.id"
+        v-for="(list, listIndex) in lists"
+        class="bg-gray-100 p-5 rounded-lg max-h-[95vh] min-w-[250px] flex flex-col"
+        :style="{ minHeight: `${Math.max(250, list.cards.length * 90)}px` }"
+      >
         <h2 class="font-medium text-md text-gray-500">{{ list.title }}</h2>
-        <draggable group="cards" :list="list.cards" item-key="id" :animation="200" class="flex flex-col">
+        <draggable
+          group="cards"
+          :list="list.cards"
+          item-key="id"
+          :animation="200"
+          class="flex flex-col h-[100%] overflow-y-auto bor"
+        >
           <template #item="{ element }">
-            <div class="bg-white p-3 my-2 rounded cursor-pointer">
+            <div @click="openModal(listIndex, element)" class="bg-white p-3 my-2 rounded cursor-pointer">
               <span class="text-md font-medium">{{ element.title }}</span>
               <p class="text-sm text-gray-500">{{ element.description }}</p>
             </div>
           </template>
         </draggable>
 
-        <button @click="openModal"
-          class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left">
+        <button
+          @click="openModal(listIndex)"
+          class="w-full p-2 rounded cursor-pointer text-sm font-medium text-gray-500 hover:bg-gray-200 text-left"
+        >
           + Add Card
         </button>
       </div>
     </div>
-    <ModalDialog :isOpen="isModalOpen" @close="closeModal" />
+    <ModalDialog :isOpen="isModalOpen" :card="editingCard" :mode="modalMode" @close="closeModal" @save="saveCard" />
   </main>
 </template>
--- src/components/ModalDialog.vue
+++ src/components/ModalDialog.vue
@@ -2,15 +2,40 @@
 import { ref, watch, nextTick } from 'vue';
 import { useFocusTrap } from '@vueuse/integrations/useFocusTrap'
 import { onClickOutside } from '@vueuse/core';
+import type { Card } from '@/types';
 
-const props = defineProps<{ isOpen: boolean }>()
-const emit = defineEmits<{ (e: 'close'): void }>()
+const props = defineProps<{
+  isOpen: boolean,
+  card: Card | null,
+  mode: 'add' | 'edit'
+}>()
+const emit = defineEmits<{
+  (e: 'close'): void,
+  (e: 'save', card: Card): void
+}>()
 
 const titleInput = ref<HTMLInputElement | null>(null)
 const targetModal = ref<HTMLDivElement | null>(null)
 const targetCardModal = ref<HTMLDivElement | null>(null)
+const localCard = ref<Card>({
+  id: 0,
+  title: '',
+  description: ''
+})
 const {activate, deactivate} = useFocusTrap(targetModal)
 const stopClickOutside = ref<(() => void) | undefined>()
+
+watch(()=>props.card, (newCard) =>{
+  if(newCard){
+    localCard.value = {...newCard}
+  } else {
+    localCard.value = {
+      id: 0,
+      title: '',
+      description: ''
+    }
+  }
+},{ immediate: true })
 
 watch(() => props.isOpen, async (isOpen) => {
   if (isOpen) {
@@ -30,21 +55,20 @@ watch(() => props.isOpen, async (isOpen) => {
     }
   }
 })
-
 </script>
 
 <template>
   <div ref="targetModal" v-if="isOpen" @keydown.esc="emit('close')" class="fixed inset-0 bg-black/30 flex items-center justify-center">
     <div ref="targetCardModal" class="bg-white p-5 rounded max-w-md w-full">
-      <h2 class="font-medium text-lg">Add new Card</h2>
-      <input type="text" ref="titleInput"  placeholder="Title" class="w-full p-2 my-2 border rounded" />
-      <textarea placeholder="Description" class="w-full p-2 my-2 border rounded"></textarea>
+      <h2 class="font-medium text-lg">{{ mode === 'add' ? 'Add new card' : 'Edit card' }}</h2>
+      <input v-model="localCard.title" type="text" ref="titleInput"  placeholder="Title" class="w-full p-2 my-2 border rounded" />
+      <textarea v-model="localCard.description" placeholder="Description" class="w-full p-2 my-2 border rounded"></textarea>
       <div class="flex justify-end gap-2">
         <button @click="emit('close')" class="text-red-500 p-2 rounded hover:bg-gray-200">
           Cancel
         </button>
-        <button  class="bg-blue-500 text-white p-2 rounded cursor-pointer hover:bg-blue-700">
-          Save
+        <button @click="emit('save', localCard)" class="bg-blue-500 text-white p-2 rounded cursor-pointer hover:bg-blue-700">
+          {{ mode === 'add' ? 'Add Card' : 'Save Changes' }}
         </button>
       </div>
     </div>
